deb694c6fc90ef72eb46fdbe46e6d02b7e2b9b24
ukakit <106052696+ukakit@users.noreply.github.com>
2022-08-04 16:33:36 -0700

    reviews controller

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -3,24 +3,84 @@ const router = express.Router()
 
 const FoodTruck = require('../db/models/FoodTruck');
 
-router.get('/:foodTruckId', async (req, res, next) => {
-    try {
-      // find the restaurant first
-      const foodTruck = await FoodTruck.findById(req.params.foodTruckId)
-  
-      if (foodTruck) {
-        // respond with all of the reviews for that restaurant
-        res.json(foodTruck.reviews)
-      } else {
-        // if you can't find it, send a 404
-        res.sendStatus(404)
-      }
-    } catch (err) {
-      next(err)
-    }
-  })
-  
+router.get('/:foodTruckId',(req,res,next)=>{
+    FoodTruck.findById(req.params.foodTruckId)
+    .populate('reviews.author')
+    .then ((foodTruck)=>{
+        if (foodTruck){
+            res.json(foodTruck.reviews)
+        } else {
+            res.sendStatus(404)
+        }
+        
+    })
+    .catch(next)
+})
 
-router.get('/:foodTruckId/:reviewId', (req, res, next) =>)
+router.get('/:foodTruckId/:reviewId', (req, res, next) =>{
+    FoodTruck.findById(req.params.foodTruckId)
+    .populate('reviews.author')
+    .then((foodTruck) => {
+        if(foodTruck) {
+            const foundReview=foodTruck.reviews.find(review=>review._id.toString()=== req.params.reviewId)
+            if (foundReview) {
+                res.json(foundReview)
+            }
+            else {
+                res.sendStatus(404)
+            }
+        } else 
+            res.sendStatus(404)
+    })
+    .catch(next)
+})
+
+
+router.post('/', (req, res, next) => {
+    const foodTruckId = req.body.foodTruckId;
+    FoodTruck.findById(foodTruckId)
+    .then((foodTruck) => {
+        if (foodTruck) {
+            foodTruck.reviews.push(req.body)
+            return foodTruck.save()
+        } else {
+            res.sendStatus(404)
+        }})
+    .then((foodTruck) => res.status(201).json( { foodTruck: foodTruck } ))
+    .catch(next)
+})
+
+router.delete('/:foodTruckId/:reviewId',(req, res, next)=>{
+    FoodTruck.findById(req.params.foodTruckId)
+    .then((foodTruck)=>{
+        if(foodTruck){
+            foodTruck.reviews.id(req.params.reviewId).remove()
+            foodTruck.save()
+            res.sendStatus(204)
+        } else {
+            res.sendStatus(404)
+        }
+    })
+    .catch(next)
+})
+
+router.put('/:reviewId', (req, res, next) => {
+    const reviewId = req.params.reviewId
+    FoodTruck.findOne({
+        'reviews._id': id,
+    })
+    .then((foodTruck)=>{
+        if (foodTruck) {
+            const review = foodTruck.reviews.id(reviewId)
+            review.set(req.body)
+            return foodTruck.save()
+        } else {
+            res.sendStatus(404)
+        }
+    })
+    .then((foodTruck)=>res.status(201).json( { foodTruck: foodTruck } ))
+    .catch(next)
+})
+
 
 module.exports = router;
