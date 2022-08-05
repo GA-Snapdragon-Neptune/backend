8cbaa5fc5cc44c87650daff7e08411c7a9f19d80
ukakit <106052696+ukakit@users.noreply.github.com>
2022-08-05 00:31:27 -0700

    added middleware and fixed error catching

--- index.js
+++ index.js
@@ -2,12 +2,16 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-// placeholder for controller import
+const morgan = require('morgan');
+const helmet = require("helmet");
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
+app.use(morgan(':date - :method (:url) :status - :response-time ms'));
+app.use(helmet());
+
 
 app.set('port', process.env.PORT || 3000);
 
@@ -25,6 +29,12 @@ app.use('/reviews', reviewsController);
 
 const usersController = require('./controllers/usersController');
 app.use('/users', usersController);
+
+app.use((err, req, res, next) => {
+	const statusCode = res.statusCode || 500;
+	const message = err.message || 'Internal Server Error';
+	res.status(statusCode).send(message);
+  });
 
 app.listen(app.get('port'), () => {
 	console.log('connected');
