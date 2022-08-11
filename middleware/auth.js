a58c49886d11ed0c8a69077f0e8d4a9d5615c113
ukakit <106052696+ukakit@users.noreply.github.com>
2022-08-10 22:57:02 -0700

    edit error message

--- middleware/auth.js
+++ middleware/auth.js
@@ -33,7 +33,7 @@ const secret =
             !req.body.password ||
             !bcrypt.compareSync(req.body.password, user.password)
         ) {
-            const err = new Error('The provided username or password is incorrect');
+            const err = new Error('The provided username, email or password is incorrect');
             err.statusCode = 422;
             throw err;
         }
